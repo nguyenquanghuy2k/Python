ba74c5ef8ca9b3e4cbe1640521dbe331ae3f9db2
nguyenquanghuy2k <102009242+nguyenquanghuy2k@users.noreply.github.com>
2022-03-23 17:39:54 +0700

    Python
    23/3

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 # arduino_LED_user.py
 
+from struct import pack
 import serial 
 import time
 from tkinter import *
@@ -8,33 +9,55 @@ from tkinter import *
 # Ensure the 'COM#' corresponds to what was seen in the Windows Device Manager
 ser = serial.Serial('COM8', 9600)
 root = Tk()
-root.title("Simple Calculator") #tieu de phan mem
+root.title("LCD-MITSUTOYO") #tieu de phan mem
+Mylabel1 =  Label(root, text = "TOWA VIET NAM",font=("Courier", 44)).grid(row=0,column=1)
+Mylabel2 =  Label(root, text = "Gia tri min",font=("Courier", 25)).grid(row=1,column=0)
+Mylabel3 =  Label(root, text = "Gia tri max",font=("Courier", 25)).grid(row=2,column=0)
+min1 = Entry(root,width=10, borderwidth=5,font=("Courier", 20))
+min1.grid(row=1, column=2, columnspan=3)
+max2 = Entry(root,width=10, borderwidth=5,font=("Courier", 20))
+max2.grid(row=2, column=2, columnspan=3)
 
-e = Entry(root,width=450, borderwidth=250)
-e.grid(row=0, column=0, columnspan=3, padx= 10, pady= 10)
+def myClick():
+    cmin=min1.get()
+    type(cmin)
+    print("gia tri min:",cmin)
+    time.sleep(0.1)
+    ser.write(float(cmin))
+    cmax=max2.get()
+    print("gia tri max:",cmax)
+    time.sleep(0.1)
+    ser.write(cmax)
 
 def led_on_off():
     user_input = input("\n Type on / off / quit : ")
     if user_input =="on":
         print("LED is on...")
         time.sleep(0.1) 
-        ser.write(b'H') 
+        ser.write(b'A') 
         led_on_off()
     elif user_input =="off":
         print("LED is off...")
         time.sleep(0.1)
-        ser.write(b'L')
+        ser.write(b'B')
         led_on_off()
     elif user_input =="quit" or user_input == "q":
         print("Program Exiting")
         time.sleep(0.1)
-        ser.write(b'L')
+        ser.write(b'B')
         ser.close()
     else:
         print("Invalid input. Type on / off / quit.")
         led_on_off()
+button1=Button(root,text='ON',padx= 30, pady=10,fg='#6699FF',bg='#66FF99',command=myClick) 
+button2=Button(root,text='OFF',padx= 30, pady=10,fg='#6699FF',bg='#66FF99',command=led_on_off)
+button3=Button(root,text='QUIT',padx= 30, pady=10,fg='#6699FF',bg='#66FF99',command=led_on_off)
 
-time.sleep(2) # wait for the serial connection to initialize
+button1.grid(row=3,column=0)
+button2.grid(row=3,column=1)
+button3.grid(row=3,column=2)
 
-led_on_off()
+time.sleep(2) # wait for the serial connection to initialize 
+
+
 root.mainloop() # vong lap de man hinh luon xuat hien
